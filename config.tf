4b8ae44b573380ebefac9de49a06642576b1d7ce
liamfit <lum.fitzpatrick@gmail.com>
2022-12-21 08:30:45 +0000

    hardcode backend config for now

--- config.tf
+++ config.tf
@@ -11,6 +11,8 @@ terraform {
     key            = "state/terraform.tfstate"
     encrypt        = true
     dynamodb_table = "terraform-state"
+    bucket         = "terraform-state-20221220164019831100000001"
+    role_arn       = "arn:aws:iam::629239571689:role/OrganizationAccountAccessRole"
   }
 }
 
--- sandbox-backend-config.tfvars
+++
@@ -1,2 +0,0 @@
-bucket   = "terraform-state-20221220164019831100000001"
-role_arn = "arn:aws:iam::629239571689:role/OrganizationAccountAccessRole"
