132822b6a7bb2ff48b51be56173d23dc20908874
liamfit <lum.fitzpatrick@gmail.com>
2022-12-21 08:21:11 +0000

    use hashicorp actions

--- variables.tf
+++ variables.tf
@@ -15,7 +15,7 @@ variable "aws_deployment_role" {
 
 variable "application_repo" {
   type    = string
-  default = "liamfit/sample-app"  
+  default = "liamfit/sample-app"
 }
 
 variable "vpc_id" {
@@ -23,9 +23,9 @@ variable "vpc_id" {
   default = "vpc-0e773070b80f39124"
 }
 variable "private_subnets" {
-  type    = list(any)
+  type = list(any)
   default = [
-      "subnet-011befea65be68a01",
-      "subnet-05b1705e0933071f9"
+    "subnet-011befea65be68a01",
+    "subnet-05b1705e0933071f9"
   ]
 }
