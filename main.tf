ad361af571ce19b52f42b64a3da434241ab18a6f
liamfit <lum.fitzpatrick@gmail.com>
2023-01-05 08:55:39 +0000

    remove proxy integration to alb

--- main.tf
+++ main.tf
@@ -104,22 +104,22 @@ resource "aws_apigatewayv2_api" "apigw_http_endpoint" {
   protocol_type = "HTTP"
 }
 
-# Create the API Gateway HTTP_PROXY integration between the created API and the private load balancer via the VPC Link.
-# Ensure that the 'DependsOn' attribute has the VPC Link dependency.
-# This is to ensure that the VPC Link is created successfully before the integration and the API GW routes are created.
-resource "aws_apigatewayv2_integration" "apigw_integration" {
-  api_id           = aws_apigatewayv2_api.apigw_http_endpoint.id
-  integration_type = "HTTP_PROXY"
-  integration_uri  = aws_lb_listener.ecs_alb_listener.arn
+# # Create the API Gateway HTTP_PROXY integration between the created API and the private load balancer via the VPC Link.
+# # Ensure that the 'DependsOn' attribute has the VPC Link dependency.
+# # This is to ensure that the VPC Link is created successfully before the integration and the API GW routes are created.
+# resource "aws_apigatewayv2_integration" "apigw_integration" {
+#   api_id           = aws_apigatewayv2_api.apigw_http_endpoint.id
+#   integration_type = "HTTP_PROXY"
+#   integration_uri  = aws_lb_listener.ecs_alb_listener.arn
 
-  integration_method     = "ANY"
-  connection_type        = "VPC_LINK"
-  connection_id          = aws_apigatewayv2_vpc_link.vpclink_apigw_to_alb.id
-  payload_format_version = "1.0"
-  depends_on = [aws_apigatewayv2_vpc_link.vpclink_apigw_to_alb,
-    aws_apigatewayv2_api.apigw_http_endpoint,
-  aws_lb_listener.ecs_alb_listener]
-}
+#   integration_method     = "ANY"
+#   connection_type        = "VPC_LINK"
+#   connection_id          = aws_apigatewayv2_vpc_link.vpclink_apigw_to_alb.id
+#   payload_format_version = "1.0"
+#   depends_on = [aws_apigatewayv2_vpc_link.vpclink_apigw_to_alb,
+#     aws_apigatewayv2_api.apigw_http_endpoint,
+#   aws_lb_listener.ecs_alb_listener]
+# }
 
 # API GW route with ANY method
 resource "aws_apigatewayv2_route" "apigw_route" {
