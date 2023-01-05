4cfc69199af356771366d628bd4ca281a7ebcd5e
liamfit <lum.fitzpatrick@gmail.com>
2023-01-05 08:52:00 +0000

    remove alb, listener and target group

--- main.tf
+++ main.tf
@@ -59,32 +59,32 @@ resource "aws_security_group_rule" "sg_ingress_rule_ecs_cluster_from_lb" {
   source_security_group_id = aws_security_group.lb_security_group.id
 }
 
-# Create the internal application load balancer (ALB) in the private subnets.
-resource "aws_lb" "ecs_alb" {
-  load_balancer_type = "application"
-  internal           = true
-  subnets            = var.private_subnets
-  security_groups    = [aws_security_group.lb_security_group.id]
-}
+# # Create the internal application load balancer (ALB) in the private subnets.
+# resource "aws_lb" "ecs_alb" {
+#   load_balancer_type = "application"
+#   internal           = true
+#   subnets            = var.private_subnets
+#   security_groups    = [aws_security_group.lb_security_group.id]
+# }
 
-# Create the ALB target group for ECS.
-resource "aws_lb_target_group" "alb_ecs_tg" {
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "ip"
-  vpc_id      = var.vpc_id
-}
+# # Create the ALB target group for ECS.
+# resource "aws_lb_target_group" "alb_ecs_tg" {
+#   port        = 80
+#   protocol    = "HTTP"
+#   target_type = "ip"
+#   vpc_id      = var.vpc_id
+# }
 
-# Create the ALB listener with the target group.
-resource "aws_lb_listener" "ecs_alb_listener" {
-  load_balancer_arn = aws_lb.ecs_alb.arn
-  port              = "80"
-  protocol          = "HTTP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_ecs_tg.arn
-  }
-}
+# # Create the ALB listener with the target group.
+# resource "aws_lb_listener" "ecs_alb_listener" {
+#   load_balancer_arn = aws_lb.ecs_alb.arn
+#   port              = "80"
+#   protocol          = "HTTP"
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.alb_ecs_tg.arn
+#   }
+# }
 
 # Create the ECS Cluster and Fargate launch type service in the private subnets
 resource "aws_ecs_cluster" "ecs_cluster" {
